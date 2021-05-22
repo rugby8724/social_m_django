221615ff49fa87c1c904e1931c5bba3e941eb0ec
Jeremiah <jeremiah.d.wise@gmail.com>
2021-05-22 15:26:43 -0500

    Add Post model and group list/form/detail/base html

--- socialm/groups/views.py
+++ socialm/groups/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
+from django.urls import reverse
+from django.views import generic
+from .models import Group, GroupMember
 
-# Create your views here.
+
+class CreateGroup(LoginRequiredMixin, PermissionRequiredMixin):
+    fields = ('name', 'description')
+    model = Group
+
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+
+class ListGroup(generic.ListView):
+    model = Group
+
+
+
